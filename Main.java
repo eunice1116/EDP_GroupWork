72126a633e6bcbde4dd6e5a3b2cebe8c80128376
Eunice <eunice16deleon@gmail.com>
2026-01-06 22:53:40 +0800

    Uploaded Main java file

---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main{
+    class Book{
+        String book;
+        String author;
+        float price;
+    }
+
+    void displayDetails(){}
+
+
+
+   public static void main(String[] args) {
+
+    }
+}
